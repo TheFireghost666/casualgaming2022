fc8a86aa2bf900f981c037724f1578e3fddab554
Benjamin Dupuis <benjamin@thanaplan.rip>
2022-01-08 14:49:38 +0100

    [+] Gun and Shooting

---
+++ Assets/Src/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Animator _animator;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+       //     Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    _animator.SetBool("shooting", true);
+                    break;
+                case TouchPhase.Ended:
+                    _animator.SetBool("shooting", false);
+                    break;
+            }
+
+
+        }
+    }
+}
