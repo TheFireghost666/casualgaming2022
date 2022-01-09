87a7a485a775b89593c87e70054bf119e64e8384
Benjamin Dupuis <benjamin@thanaplan.rip>
2022-01-10 00:32:08 +0100

    [+] shooting sound & target detroying

--- Assets/Src/Scripts/Gun.cs
+++ Assets/Src/Scripts/Gun.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public Camera _cam;
     public Animator _animator;
     public AudioSource _shootingSound;
+
+    void Start()
+    {
+    }
 
     void Update()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-       //     Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-            
+
             switch (touch.phase)
             {
                 case TouchPhase.Began:
                     _animator.SetBool("shooting", true);
                     _shootingSound.Play();
+
+/*
+                    Vector3 posFar = new Vector3(touch.position.x, touch.position.y, Camera.main.farClipPlane);
+                    Vector3 posNear = new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane);
+                    Vector3 posF = _cam.ScreenToWorldPoint(posFar);
+                    Vector3 posN = _cam.ScreenToWorldPoint(posNear);
+  */                Ray ray;
+                    RaycastHit hit;
+                    ray = _cam.ScreenPointToRay(touch.position);
+                    if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                    {
+                        Debug.Log("touching " + hit.transform.gameObject.name);
+                        if (hit.transform.gameObject.tag == ("Target"))
+                        {
+                            Destroy(hit.transform.gameObject);
+                        }
+                        else
+                        {
+                            Debug.Log("This object is not the target");
+                        }
+                    } else
+                    {
+                        Debug.Log("touching the void");
+                    }
+                    
+                    
                     break;
                 case TouchPhase.Ended:
                     _animator.SetBool("shooting", false);
--- Assets/shootTarget.cs
+++ Assets/shootTarget.cs
@@ -13,6 +13,7 @@ public class shootTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
 }
